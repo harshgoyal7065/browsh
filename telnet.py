4ad90572ab532685907810b48a19d31445f3de2c
Harsh Goyal <harsh.goyal@joshtechnologygroup.com>
2025-02-01 23:55:06 +0530

    Added Scrolling effect

--- telnet.py
+++ telnet.py
@@ -67,6 +67,8 @@ class URL:
         return content
 
 WIDTH, HEIGHT = 800, 600
+SCROLL_STEP = 100
+HSTEP, VSTEP = 13, 18
 
 def lex(body):
     text = ""
@@ -79,6 +81,17 @@ def lex(body):
         elif not in_tag:
             text += c
     return text
+
+def layout(text):
+    display_list = []
+    cursor_x, cursor_y = HSTEP, VSTEP
+    for c in text:
+        display_list.append((cursor_x, cursor_y, c))
+        cursor_x += HSTEP
+        if cursor_x >= WIDTH - HSTEP:
+            cursor_y += VSTEP
+            cursor_x = HSTEP
+    return display_list
 class Browser:
     def __init__(self):
         self.window = tkinter.Tk()
@@ -88,18 +101,25 @@ class Browser:
             height=HEIGHT
         )
         self.canvas.pack()
+        self.scroll = 0
+        self.window.bind("<Down>", self.scrolldown)
+
+    def scrolldown(self, e):
+        self.scroll += SCROLL_STEP
+        self.draw()
+
+    def draw(self):
+        self.canvas.delete("all")
+        for x, y, c in self.display_list:
+            if y > self.scroll + HEIGHT: continue
+            if y + VSTEP < self.scroll: continue
+            self.canvas.create_text(x, y - self.scroll, text=c)
 
     def load(self, url):
         body = url.request()
         text = lex(body)
-        HSTEP, VSTEP = 13, 18
-        cursor_x, cursor_y = HSTEP, VSTEP
-        for c in text:
-            self.canvas.create_text(cursor_x, cursor_y, text=c)
-            cursor_x += HSTEP
-            if cursor_x >= WIDTH - HSTEP:
-                cursor_y += VSTEP
-                cursor_x = HSTEP
+        self.display_list = layout(text)
+        self.draw()
 
 if __name__ == "__main__":
     Browser().load(URL(sys.argv[1]))
